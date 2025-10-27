4fe2430c2971ef7cfff361baacd8b058747ce914
Adarshswagh <waghmareadarsh1432@gmail.com>
2025-10-27 17:43:52 +0530

    keyword add on project page

--- src/app/wyce-exclucity/page.tsx
+++ src/app/wyce-exclucity/page.tsx
@@ -9,7 +9,7 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Buy Luxury Flats in Bavdhan, Pune | WYCE Exclucity",
   description: "WYCE Exclucity in Bavdhan, Pune offers spacious 2, 3 & 4 BHK luxury flats with modern amenities, excellent connectivity, and a premium lifestyle.",
-  keywords: "flats in bavdhan, 2 bhk flats in bavdhan, luxury flats in bavdhan, 3 bhk flats in Bavdhan, 4 bhk flats in Bavdhan, flats for sale in bavdhan, premium homes near kothrud, flats in Pune",
+  keywords: "wyce exclucity, flats in bavdhan, 2 bhk flats in bavdhan, luxury flats in bavdhan, 3 bhk flats in Bavdhan, 4 bhk flats in Bavdhan, flats for sale in bavdhan, premium homes near kothrud, flats in Pune",
 
 };
 
