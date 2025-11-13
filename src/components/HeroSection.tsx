9ddabb659b424c9a022bdefca4d4817836f71a36
Adarshswagh <waghmareadarsh1432@gmail.com>
2025-11-13 12:14:26 +0530

    banner edit

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,7 +4,7 @@ import laptopBg from '../../public/images/laptop.jpg'
 
 export default function HeroSection() {
     return (
-        <div className="relative bg-cover bg-center bg-no-repeat text-white  bg-black/100" style={{ backgroundImage: "url('/images/bg-image.png')" }}>
+        <div className="relative bg-cover bg-center bg-no-repeat text-white  bg-black/100">
             {/* Social Sidebar - Outside the banner */}
             <div className="absolute left-2 sm:left-4 lg:left-8 top-1/2 -translate-y-1/2 flex-col gap-6 sm:gap-8 lg:gap-10 z-[5] hidden md:flex">
 
@@ -12,7 +12,7 @@ export default function HeroSection() {
 
 
             {/* Banner Container */}
-            <div className="relative w-full h-[750px] sm:h-[700px] md:h-[780px] flex items-center justify-center overflow-hidden"
+            <div className="relative w-full h-[750px] sm:h-[700px] md:h-screen flex items-center justify-center overflow-hidden"
                 style={{
                     backgroundImage: `url(${phoneBg.src})`,
                     backgroundPosition: 'center',
@@ -31,7 +31,7 @@ export default function HeroSection() {
                 
                 {/* Laptop/Desktop background */}
                 <div 
-                    className="absolute inset-0 hidden lg:block"
+                    className="absolute inset-0 hidden lg:block "
                     style={{
                         backgroundImage: `url(${laptopBg.src})`,
                         backgroundPosition: 'center',
